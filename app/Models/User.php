a99b8e9fb618177b71eb9b7d0d7a82a6b91b688e
vagrant <vagrant@homestead>
2018-04-05 11:17:16 +0000

    “将用户模型移至模型文件夹”

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -17,7 +17,7 @@ class User extends Authenticatable
     protected $fillable = [
         'name', 'email', 'password',
     ];
-
+Ó
     /**
      * The attributes that should be hidden for arrays.
      *
