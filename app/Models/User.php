ba6843a9d7e2e74a420161f44cd1532817ee0d19
vagrant <vagrant@homestead>
2018-04-06 09:16:55 +0000

    用户注册成功

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,7 +17,7 @@ class User extends Authenticatable
     protected $fillable = [
         'name', 'email', 'password',
     ];
-Ó
+
     /**
      * The attributes that should be hidden for arrays.
      *
@@ -26,4 +26,10 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
-}
+    
+      public function gravatar($size = '100')
+    {
+        $hash = md5(strtolower(trim($this->attributes['email'])));
+        return "http://www.gravatar.com/avatar/$hash?s=$size";
+    }
+}    
