97cf3a7c160685b6e12818af3c08fb63a62e41f7
vagrant <vagrant@homestead>
2018-04-09 04:07:55 +0000

    完成 用户 修改，更新，列表 与删除行动

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -8,6 +8,16 @@ use Auth;
 class SessionsController extends Controller
 {
     //
+
+   public function __construct()
+    {
+        $this->middleware('guest', [
+            'only' => ['create']
+        ]);
+         $this->middleware('guest', [
+            'only' => ['create']
+        ]);
+    }
     public function create()
     {
     	return  view('sessions.create');
@@ -18,17 +28,14 @@ class SessionsController extends Controller
            'email' => 'required|email|max:255',
            'password' => 'required'
        ]);
-        if (Auth::attempt($credentials,$request->has('remember'))) {
-           // 登录成功后的相关操作
-        	 session()->flash('success', '欢迎回来！');
-           return redirect()->route('users.show', [Auth::user()]);
+
+       if (Auth::attempt($credentials, $request->has('remember'))) {
+           session()->flash('success', '欢迎回来！');
+           return redirect()->intended(route('users.show', [Auth::user()]));
        } else {
-           // 登录失败后的相关操作
-       	session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
-       	 return redirect()->back();
+           session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
+           return redirect()->back();
        }
-		
-       
     }
 
      public function destroy()
